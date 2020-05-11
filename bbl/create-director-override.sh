70fd654d0989a5507e2467f3ca86342de1e01e7a
PaymentsHubRebels <63651517+PaymentsHubRebels@users.noreply.github.com>
2020-05-11 11:39:26 +0100

    Update create-director-override.sh
    creating offline package and uploading to s3

--- bbl/create-director-override.sh
+++ bbl/create-director-override.sh
@@ -6,3 +6,7 @@ prepare_vars_file_for_cpi
 set_default_cpi_flags
 
 ./bucc/bin/bucc up --cpi $(cpi) ${bucc_args}
+
+./bucc/bin/bucc offline --cpi ${cpi} --destination /tmp/offline
+
+aws s3 cp /tmp/offline/$(ls /tmp/offline)  s3://bootstrap-bbl-offline-store/
